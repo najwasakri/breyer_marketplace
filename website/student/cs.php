329b89fc41bc8a378febb1cc2dfd568372a0a7cd
najwasakri <wawasakri0406@gmail.com>
2026-04-28 13:45:01 +0800

    fix: update image format for BAJU COMPUTER SYSTEM in cs.php

--- website/student/cs.php
+++ website/student/cs.php
@@ -724,7 +724,7 @@ if (!isset($_SESSION['user_id'])) {
                 [
                     'type' => 'BAJU COMPUTER SYSTEM',
                     'price' => 'RM85.00',
-                    'image' => 'ads8/breyer-baju1.JPEG'
+                    'image' => 'ads8/breyer-baju1.png'
                 ],
                 [
                     'type' => 'BAJU T-SHIRT KOLEJ',
