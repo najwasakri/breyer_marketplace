0a541be35ff3bb48a6b265a73b3821770220afc4
najwasakri <wawasakri0406@gmail.com>
2026-04-28 14:30:30 +0800

    SAVE

--- website/student/electrical.php
+++ website/student/electrical.php
@@ -629,12 +629,12 @@ if (!isset($_SESSION['user_id'])) {
                 [
                     'type' => 'BAJU ELECTRICAL',
                     'price' => 'RM85.00',
-                    'image' => 'ads8/breyer-baju1.png'
+                    'image' => 'ads46/BAJU ELECTRICAL.png'
                 ],
                 [
                     'type' => 'BAJU T-SHIRT KOLEJ',
                     'price' => 'RM28.00',
-                    'image' => 'ads8/breyer-baju1.png'
+                    'image' => 'ads42/BAJU T-SHIRTT.png'
                 ]
             ];
 
